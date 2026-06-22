1a845ac7e19b81f2a24c5d3dd125526279e695c3
DIVYADHARSHINI S <divyadharshinis1610@gmail.com>
2026-06-22 12:09:10 +0530

    Add new routes for Dashboard and Admin pages

--- App.jsx
+++ App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import StudentRegister from "./pages/StudentRegister";
 import CompanyRegister from "./pages/CompanyRegister";
+import Dashboard from "./pages/Dashboard";
+import CompanyLogin from "./pages/CompanyLogin";
+import CompanyDashboard from "./pages/CompanyDashboard";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/student-register" element={<StudentRegister />} />
         <Route path="/company-register" element={<CompanyRegister />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/company-login" element={<CompanyLogin />} />
+        <Route path="/company-dashboard" element={<CompanyDashboard />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
